9100fc6874446435c69326ac6413fb7082241e09
nicrozzy <94608718+nicrozj@users.noreply.github.com>
2024-11-01 14:12:56 +0300

    Delete .nuxt directory

--- .nuxt/dist/server/_nuxt/entry-styles.C77ms831.mjs
+++
@@ -1,2 +0,0 @@
-import style_0 from './entry-styles-1.mjs-B-bQ_Ijt.js';
-export default [style_0]
--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/igorzorkin/Documents/projects/vue-js/money-tracker/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
